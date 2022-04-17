60526d87df03dc19dac6390b353bf07a32969629
Renam Bulhoes <renamgustavo@live.com>
2022-04-17 17:31:05 -0300

    insert City ok

--- src/main/java/com/devsuperior/bds02/services/CityService.java
+++ src/main/java/com/devsuperior/bds02/services/CityService.java
@@ -6,6 +6,7 @@ import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.bds02.dto.CityDTO;
 import com.devsuperior.bds02.entities.City;
@@ -17,9 +18,20 @@ public class CityService {
 	@Autowired
 	private CityRepository repository;
 	
+	@Transactional(readOnly = true)
 	public List<CityDTO> findAll(){
 		List<City> list = repository.findAll(Sort.by("name"));
 		
 		return list.stream().map(x -> new CityDTO(x)).collect(Collectors.toList());
+	}
+	
+	
+	@Transactional
+	public CityDTO insert(CityDTO dto) {
+		City entity =  new City();
+		entity.setName(dto.getName());
+		entity = repository.save(entity);
+		
+		return new CityDTO(entity);
 	}
 }
